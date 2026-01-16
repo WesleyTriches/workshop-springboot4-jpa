03cd2b81f4c369e0f874d4c7026431cb02a5d5fc
Wesley Triches <wesleygtriches@gmail.com>
2026-01-16 19:24:50 -0300

    Projec created

---
+++ src/test/java/com/wesley/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesley.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
